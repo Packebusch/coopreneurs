1a4d3fb755ab029976708b4b2dd4e7503571751c
Packebusch <tom.packe@gmx.de>
2020-07-10 19:09:35 +0200

    foo

--- website/pages/en/contact.js
+++ website/pages/en/contact.js
@@ -12,45 +12,28 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const Container = CompLibrary.Container;
 
 class Contact extends React.Component {
-  constructor(props) {
+	constructor(props) {
     super(props);
-    this.state = {
-      name: '',
-      email: ''
-    };
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    console.log(event);
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
   }
-  myChangeHandler = (event) => {
-    this.setState({name: event.target.value});
-  }
+
   render() {
     return (
-      <div className="mainContainer">
-        <Container padding={['bottom', 'top']}>
-          <div className="showcaseSection">
-            <div className="prose">
-              <h1>Register to our online meetup, {this.state.name}.</h1>
-              <p>Our virtual meetup takes place at the first Wednesday every month at 8pm. Please join us, if you'd like to talk to us. It's open for everyone no matter the background.</p>
-              <form onSubmit={this.mySubmitHandler}>
-                <label>
-                  Your Email:
-                  <input type="email" name="email" />
-                </label>
-                <label>
-                  Your name:
-                  <input onChange={this.myChangeHandler} type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
-              </form>
-            </div>
-          </div>
-        </Container>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
+
 
 module.exports = Contact;
